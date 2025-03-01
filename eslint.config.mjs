c5fe2105743322c6ba92d7d5307f19c02d26d8c5
tet0-h <tetokon+github2@gmail.com>
2025-03-01 16:44:40 +0900

    ESLintの設定を更新し、TypeScriptおよびVueファイルに対するルールを追加。package.jsonにlintスクリプトを追加。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+    rules: {
+      "eqeqeq": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+    }
+  }
+);
