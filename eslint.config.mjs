43699b2bff23b6312f0f61a3b649d06a928d7c27
tet0-h <tetokon+github2@gmail.com>
2025-03-05 03:33:03 +0900

    package.jsonのTypeScriptバージョンを5.8.2から5.7.3にダウングレードし、ESLint設定をNuxtのルールに合わせて更新

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,26 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
-    rules: {
-      "eqeqeq": "error",
-      "@typescript-eslint/no-floating-promises": "error",
-    }
+  // {
+  //   files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+  //   rules: {
+  //     "eqeqeq": "error",
+  //     "@typescript-eslint/no-floating-promises": "error",
+  //   }
+  // }
+).override("nuxt/javascript", {
+  rules: {
+    "eqeqeq": "error",
   }
-);
+}
+).override("nuxt/typescript/rules", {
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-floating-promises": "error",
+  }
+});
