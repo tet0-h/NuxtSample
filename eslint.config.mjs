58cc741ba3570722bb2dce70913dd601c98a77bc
tet0-h <51284198+tet0-h@users.noreply.github.com>
2025-03-04 19:28:21 +0000

    Apply Prettier Change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs';
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-).override("nuxt/javascript", {
-  rules: {
-    "eqeqeq": "error",
-  }
-}
-).override("nuxt/typescript/rules", {
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+export default withNuxt()
+// Your custom configs here
+  .override("nuxt/javascript", {
+    rules: {
+      eqeqeq: "error",
     },
-  },
-  rules: {
-    "@typescript-eslint/no-floating-promises": "error",
-  }
-});
+  })
+  .override("nuxt/typescript/rules", {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  });
