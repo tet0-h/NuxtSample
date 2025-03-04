1e23dc1aac3b459b0929962f9241395a465615e7
tet0-h <tetokon+github2@gmail.com>
2025-03-05 05:12:44 +0900

    VitestのGitHub Actionsワークフローを追加し、ユニットテストのカバレッジレポートを設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
+    coverage: {
+      reporter: ["html", "text", "json-summary", "json"],
+    },
   },
 });
