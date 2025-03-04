4805ded58eb8e8e40f67780af34baf7be03e5917
tet0-h <tetokon+github2@gmail.com>
2025-03-05 04:21:10 +0900

    テスト環境を設定し、ユニットテストを追加。ESLintとVitestの設定を更新。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,6 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
   // Your custom configs here
-  // {
-  //   files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
-  //   rules: {
-  //     "eqeqeq": "error",
-  //     "@typescript-eslint/no-floating-promises": "error",
-  //   }
-  // }
 ).override("nuxt/javascript", {
   rules: {
     "eqeqeq": "error",
