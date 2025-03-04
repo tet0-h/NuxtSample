e2615e77f756a8c67f04aac142d7929dbfda3071
tet0-h <51284198+tet0-h@users.noreply.github.com>
2025-03-04 18:34:30 +0000

    Apply Prettier Change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs';
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-  // {
-  //   files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
-  //   rules: {
-  //     "eqeqeq": "error",
-  //     "@typescript-eslint/no-floating-promises": "error",
-  //   }
-  // }
-).override("nuxt/javascript", {
-  rules: {
-    "eqeqeq": "error",
-  }
-}
-).override("nuxt/typescript/rules", {
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+export default withNuxt()
+// Your custom configs here
+// {
+//   files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+//   rules: {
+//     "eqeqeq": "error",
+//     "@typescript-eslint/no-floating-promises": "error",
+//   }
+// }
+  .override("nuxt/javascript", {
+    rules: {
+      eqeqeq: "error",
     },
-  },
-  rules: {
-    "@typescript-eslint/no-floating-promises": "error",
-  }
-});
+  })
+  .override("nuxt/typescript/rules", {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  });
